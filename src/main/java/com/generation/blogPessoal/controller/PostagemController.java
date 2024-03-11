15b06b53560e3760bce7e4e8c4cc6c4effca97e0
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-03-11 13:02:14 -0300

    feat - crud postagem

--- src/main/java/com/generation/blogPessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogPessoal/controller/PostagemController.java
@@ -1,48 +1,80 @@
 package com.generation.blogPessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogPessoal.model.Postagem;
 import com.generation.blogPessoal.repository.PostagemRepository;
 
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagens")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
-	
+
 	@Autowired
-	private PostagemRepository postagemrepository;
-	
+	private PostagemRepository postagemRepository;
+
 	@GetMapping("/all")
 	public ResponseEntity<List<Postagem>> getAll() {
-		return ResponseEntity.ok(postagemrepository.findAll());
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Postagem> getById(@PathVariable Long id){
-		return postagemrepository.findById(id)
-				.map(resposta -> ResponseEntity.ok(resposta))
-				.orElse(ResponseEntity.notFound().build());
-		
-	}
-	
-	@PostMapping("/create")
-	public ResponseEntity<Postagem> createPostagem(@RequestBody Postagem novaPostagem) {
-	    Postagem postagemCriada = postagemrepository.save(novaPostagem);
-	    return ResponseEntity.status(HttpStatus.CREATED).body(postagemCriada);
+		return ResponseEntity.ok(postagemRepository.findAll());
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+
+	}
+
+	@GetMapping("/title/{title}")
+	public ResponseEntity<List<Postagem>> getByTitle(@Valid @PathVariable String title) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(title));
+
+	}
+
+	@PostMapping("/post")
+	public ResponseEntity<Postagem> createPostagem(@Valid @RequestBody Postagem newPost) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(newPost));
+	}
+		
+	@PutMapping("/update")
+	public ResponseEntity<Postagem> updatePost(@Valid @RequestBody Postagem updatePost) {
+	    return postagemRepository.findById(updatePost.getId())
+	    		.map(resposta -> {updatePost.setCreatedAt(resposta.getCreatedAt());
+	    		
+	        Postagem postagemAtualizada = postagemRepository.save(updatePost);
+	        return ResponseEntity.status(HttpStatus.OK).body(postagemAtualizada);
+	    }).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/delete/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+
+		if (postagem.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		postagemRepository.deleteById(id);
+	}
 
 }
--- src/main/java/com/generation/blogPessoal/model/Postagem.java
+++ src/main/java/com/generation/blogPessoal/model/Postagem.java
@@ -1,75 +1,80 @@
-	package com.generation.blogPessoal.model;
-	
-	import java.time.LocalDateTime;
-	
-	import org.hibernate.annotations.UpdateTimestamp;
-	
-	import jakarta.persistence.Entity;
-	import jakarta.persistence.Table;
-	import jakarta.validation.constraints.NotBlank;
-	import jakarta.validation.constraints.Size;
-	import jakarta.persistence.Id;
-	import jakarta.persistence.GeneratedValue;
-	import jakarta.persistence.GenerationType;
-	
-	
-	
-	
-		@Entity
-		@Table(name = "tb_postagens")
-		public class Postagem {
-		
-			@Id
-			@GeneratedValue(strategy = GenerationType.IDENTITY)
-			private Long id;
-			
-			@NotBlank(message = "o Atriburo titulo é Obrigatório")	
-			@Size(min =5, max= 100 , message ="O atriburo deve conter no mínimo 05 e no máximo 100 caracteres")
-			private String titulo;
-			
-			
-			@NotBlank(message = "o Atriburo texto é Obrigatório")	
-			@Size(min =50, message ="O atriburo deve conter no mínimo 05 caracteres")
-			private String texto;
-			
-			@UpdateTimestamp
-			private LocalDateTime data;
-		
-		public Long getId() {
-			return id;
-		}
-	
-		public void setId(Long id) {
-			this.id = id;
-		}
-	
-		public String getTitulo() {
-			return titulo;
-		}
-	
-		public void setTitulo(String titulo) {
-			this.titulo = titulo;
-		}
-	
-		public String getTexto() {
-			return texto;
-		}
-	
-		public void setTexto(String texto) {
-			this.texto = texto;
-		}
-	
-		public LocalDateTime getData() {
-			return data;
-		}
-	
-		public void setData(LocalDateTime data) {
-			this.data = data;
-		}
-		
-		
-		
-		
-		
-		
+package com.generation.blogPessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "o Atriburo titulo é Obrigatório")
+	@Size(min = 5, max = 100, message = "O atriburo deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+
+	@NotBlank(message = "o Atriburo texto é Obrigatório")
+	@Size(min = 10, message = "O atributo deve conter no mínimo 05 caracteres")
+	private String texto;
+
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+
+	public Long getId() {
+		return id;
 	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+
+
+}
